e02c92e07d0e703ca1fbb072405b48bf7aab7341
Rajat Gupta <102474088+Rajat158@users.noreply.github.com>
2022-07-18 01:02:31 +0530

    Create Operating an array.py

---
+++ Arrays/Operating an array.py
@@ -0,0 +1,42 @@
+'''
+Given an array A of size N, your task is to do some operations, i.e., search an element, insert an element, and delete an element by completing the functions. Also, all functions should return a boolean value.
+
+Note: In delete operation return true even if element is not present.
+
+Example:
+
+Input:
+5
+2 4 1 0 6
+1 2 2 0
+Output:
+1 1 1
+Your Task:
+Since this is a function problem, you only need to complete the provided functions.
+
+Constraints:
+1 <= T <= 100
+1 <= N <= 100
+0 <= Ai <= 1000
+'''
+#Solution-->
+# Your task is to complete all
+# three functions
+
+# if the element is found in the list
+# function  must return true or else
+# return false
+def searchEle(a, x):
+    # Code here
+
+# fucntion must return true if 
+# insertion is successful or else
+# return false
+def insertEle(a, y, yi):
+    # Code here
+
+# fucntion must return true if 
+# deletion is successful or else
+# return false
+def deleteEle(a, z):
+    # Code here
