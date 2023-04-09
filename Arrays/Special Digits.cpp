faa8bcf2239a1b72f206ab525f2302e1e98568e7
Rajat Gupta <102474088+Rajat158@users.noreply.github.com>
2023-04-09 19:12:24 +0530

    Update and rename Special Digits.py to Special Digits.cpp

--- Arrays/Special Digits.cpp
+++ Arrays/Special Digits.cpp
@@ -1,4 +1,4 @@
-'''
+/*
 You are given 5 integers - N,A,B,C,D.
 Let us say all the integers of length N, having only A or B in their decimal representation are good integers. Also, among all the good integers,
 all those integers whose sum of digits contains either C or D or both are best integers.
@@ -36,24 +36,47 @@ Expected Space Complexity: O(N)
 Constraints:
 1 ≤ A, B, C, D ≤ 9
 1 ≤ N ≤ 105
-'''
-class Solution:
-    def inverse(self, x, MOD):
-        return pow(x, MOD-2, MOD)
-            
-    def bestNumbers(self, N : int, A : int, B : int, C : int, D : int) -> int:
-        # code here
-        MOD = 10**9 + 7
-        fact = [1]*(N+1)
-        for i in range(1, N+1):
-            fact[i] = (fact[i-1]*i)%MOD
-        
-        res = 0
-        for x in range(N+1):
-            y = N - x
-            S = set(map(int, str(A*x + B*y)))
-            if {C,D} >= S:
-                res = (res + fact[N] * self.inverse(fact[x], MOD) * self.inverse(fact[y], MOD)) % MOD
-        
-        return res
+*/
+class Solution {
+  public:
+    bool check(int n,int c,int d) {
+        while(n) {
+            int r=n%10;
+            if(r==c || r==d) {
+                return true;
+            }
+            n/=10;
+        }
+        return false;
+    }
+    long long binpow(long long a,long long b,long long mod) {
+        long long res=1;
+        while(b) {
+            if(b&1) {
+                res=(res*a)%mod;
+            }
+            a=(a*a)%mod;
+            b>>=1;
+        }
+        return res;
+    }
+    int bestNumbers(int N, int A, int B, int C, int D) {
+        if(A==B) {
+            return check(N*A,C,D);
+        } 
+        long long mod=1e9+7,ans=0;
+        vector<long long> fac(N+1,0);
+        fac[0]=1;
+        for(long long i=1;i<=N;i++) {
+            fac[i]=(fac[i-1]*i)%mod;
+        }
+        for(int i=0;i<=N;i++) {
+            int s=i*A+(N-i)*B;
+            if(check(s,C,D)) {
+                ans=(ans+(fac[N]*binpow((fac[i]*fac[N-i])%mod,mod-2,mod))%mod)%mod;
+            }
+        }
+        return ans;
+    }
+};
         
