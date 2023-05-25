7fecf7e60c099cc037e5944fcef241d823066868
Rajat Gupta <102474088+Rajat158@users.noreply.github.com>
2023-05-25 13:41:57 +0530

    Create Expression Add Operators.py

---
+++ Arrays/Expression Add Operators.py
@@ -0,0 +1,68 @@
+'''
+Given a string S that contains only digits (0-9) and an integer target, return all possible strings to insert the binary operator ' + ', ' - ', or ' * ' between the digits of S so that the resultant expression evaluates to the target value.
+
+Note:
+
+Operands in the returned expressions should not contain leading zeros. For example, 2 + 03 is not allowed whereas 20 + 3 is fine. It is allowed to not insert any of the operators.
+
+If no solution is found, return an empty string array.
+Example 1:
+
+Input:
+S = "123"
+target = 6
+Output: { "1*2*3", "1+2+3"}
+Explanation: Both "1*2*3" and "1+2+3" evaluate to 6.
+
+Example 2:
+
+Input:
+S = "3456237490"
+target = 9191
+Output: { } 
+Explanation: There are no expressions that can be created from "3456237490"  to evaluate to 9191.
+
+Example 3:
+
+Input:
+S = "12"
+target = 12
+Output: { "12"} 
+Explanation: S itself matches the target. No other strings are possible.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function addOperators() which takes string S and integer target as parameters and returns a list of strings that contains all valid possibilities.
+
+Expected Time Complexity: O(|S|*4|S|)
+Expected Auxiliary Space: O(|s|*3|S|)
+
+Constraints:
+1 ≤ |S| ≤ 10
+S consists of only digits.
+-231 ≤ target ≤ 231-1
+'''
+#User function Template for python3
+
+class Solution:
+        def addOperators(self, s, target):
+            n = len(s)
+            ans = []
+            def dfs(i, path, result, prev):
+                if i == n:
+                    if result == target:
+                        ans.append(path)
+                    return
+    
+                for j in range(i+1, n+1):
+                    if j > i+1 and s[i] == '0': 
+                        break  
+                    num = int(s[i:j])
+                    if i == 0:
+                        dfs(j, path + str(num), result + num, num)  
+                    else:
+                        dfs(j, path + "+" + str(num), result + num, num)
+                        dfs(j, path + "-" + str(num), result - num, -num)
+                        dfs(j, path + "*" + str(num), result - prev + prev*num, prev*num)  
+    
+            ans = []
+            dfs(0, "", 0, 0)
+            return ans
