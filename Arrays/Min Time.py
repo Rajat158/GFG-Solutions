bc808eb844e43c488e4e8ba6db5b71af8a7ec7ba
Rajat Gupta <102474088+Rajat158@users.noreply.github.com>
2023-06-16 15:02:50 +0530

    Create Min Time.py

---
+++ Arrays/Min Time.py
@@ -0,0 +1,63 @@
+'''
+Given an array locations[] of size n where locations[i] represents the location of the ith fruit on the x-axis and also given an array types[]  where types[i] is an integer which represents the type of the ith fruit. You are initially at coordinate 0 and you have to collect all the fruits and then return back to coordinate 0 again. To move 1 unit on the x-axis requires 1 second of time. The only condition is that you have to collect the fruits in a non-decreasing order of their types[i] (for example if ith fruit has type[i]=1 and jth fruit has type[j]=2 then ith fruit has to be picked before jth fruit). Find minimum time to collect all fruits in the non-decreasing order of their types and return back to coordinate 0.
+
+Note: You can assume it takes no time to pick up fruit and the location of a fruit can be negative also.
+
+Example 1:
+
+Input:
+n=4
+locations={1,3,5,7}
+types={1,2,3,1}
+Output:
+18
+Explanation:
+You start at x=0 and move to x=7 and 
+in the way pick fruits of type 1 at x=1 
+and x=7 and it took total 7 seconds to 
+move from 0->7 now you move to x=3 
+in 4 seconds and than to x=5 in 2 seconds 
+and than return back to x=0 in 5 seconds.
+So overall it took 18 seconds(7+4+2+5).
+Example 2:
+
+Input:
+n=4
+locations={-4,-3,1,-8}
+types={4,2,4,5}
+Output:
+24
+Explanation:
+The optimal way is to way go to x=-3 
+in starting than 1 and follow path 
+like this  (1)->(-4)->(-8)->(0) and 
+total time it takes is 24 .
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function minTime() which takes an integer n, integer array locations and types and you have to return minimum time to collect all fruits.
+
+Expected Time Complexity: O(n)
+Expected Space Complexity: O(n)
+
+Constraints:
+1<=n<=105
+-109<=locations[i]<=109
+1<=types[i]<=105
+'''
+from typing import List
+from math import inf
+from collections import defaultdict
+class Solution:
+    def minTime(self, n : int, locations : List[int], types : List[int]) -> int:
+        mn_loc, mx_loc = defaultdict(lambda: inf), defaultdict(lambda: -inf)
+        for typ, loc in zip(types, locations):
+            mn_loc[typ] = min(mn_loc[typ], loc)
+            mx_loc[typ] = max(mx_loc[typ], loc)
+        mn_prev, mx_prev = 0, 0
+        dp = [(0, 0)]
+        for typ in sorted(mn_loc):
+            mn_cur, mx_cur = mn_loc[typ], mx_loc[typ]
+            dp.append((min(dp[-1][0] + abs(mx_cur-mn_prev), dp[-1][1] + abs(mx_cur-mx_prev)) + mx_cur - mn_cur,\
+                min(dp[-1][0] + abs(mn_cur-mn_prev), dp[-1][1] + abs(mn_cur-mx_prev)) + mx_cur - mn_cur))
+            mn_prev, mx_prev =  mn_cur, mx_cur
+            
+        return min(dp[-1][0] + abs(mn_cur), dp[-1][1] + abs(mx_cur))
